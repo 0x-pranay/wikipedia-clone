edf1907f33aa4cf18549bb7b64e3f62ccab3e607
velisoju pranay <velisojupranay@gmail.com>
2019-11-02 23:40:13 +0530

    added views/templates for registration and login

--- wikipediaApp/controllers/userController.js
+++ wikipediaApp/controllers/userController.js
@@ -1,9 +1,14 @@
 // Weclome page for users
-exports.index = (req, res) => res.send('Welcome User');
+exports.index = (req, res) => {
+    res.send('Welcome User')};
 
 // Register page for users
-exports.users_register = (req, res) => res.send('Users Register Page');
+exports.users_register = (req, res) => {
+    res.render('register', { title: "Register"});
+};
 
 // Login page for users.
-exports.users_login = (req, res) => res.send('Users Login Page');
+exports.users_login = (req, res) => {
+    res.render('login', { title: "Login"});
+};
 
